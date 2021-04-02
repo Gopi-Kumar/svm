30a12ee316fbb719c79361a7715f3f131c837410
Gopi Kumar Kaushik <gopikumarkaushik9065@gmail.com>
2021-04-03 00:47:33 +0530

    class 8 math relational number chapter added

--- src/classVideos/class8/math8.js
+++ src/classVideos/class8/math8.js
@@ -1,6 +1,30 @@
+const videoData = [
+    {
+        chapter:1,
+        name : "परिमेय संख्याएँ",
+        link:["T8PO6l0lqbs","Fkq2UPkvDwo","mKbqiW9co3M","FUKhXF4nx6Y","QTHqD2HkGxU","6snpHoIXr5U","aPAbFsevLGg","5AZ2SD1Os6M","8rlyw8HEs0g"],
+    },
+]
 const math8 = () => {
+
     return (
-        <h1>Class 8 Math</h1>
+        <div className="classes">
+          {videoData.map(data => {
+            return (
+                <div key={data.chapter} className="chapter">
+                    <p className="chapter_name">{data.chapter}.{data.name}</p>
+                    <div className="class_list">
+                        {data.link.map(link => {
+                            return (
+                                <iframe key = { Math.random()}  src={`https://www.youtube.com/embed/${link}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"    allowFullScreen></iframe>
+                            )
+                        })}
+                    </div>
+
+                </div>
+            )
+         })}
+        </div>
     )
 }
 export default math8;
